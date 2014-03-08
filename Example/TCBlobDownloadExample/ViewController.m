37aa01de18c59cacf92f07639f4678ea2564cb08
thibaultCha <thibaultcha@me.com>
2014-03-08 15:58:00 +0100

    Updated example project to compile in 64bits

--- Example/TCBlobDownloadExample/ViewController.m
+++ Example/TCBlobDownloadExample/ViewController.m
@@ -48,12 +48,11 @@
                                        firstResponse:NULL
                                             progress:^(float receivedLength, float totalLength, NSInteger remainingTime) {
                                                 if (remainingTime != -1) {
-                                                    [self.remainingTime setText:[NSString stringWithFormat:@"%ds", remainingTime]];
+                                                    [self.remainingTime setText:[NSString stringWithFormat:@"%lds", (long)remainingTime]];
                                                 }
                                             }
                                                error:NULL
                                             complete:^(BOOL downloadFinished, NSString *pathToFile) {
-                                                NSLog(@"called");
                                                 NSString *str = downloadFinished ? @"Completed" : @"Cancelled";
                                                 [self.remainingTime setText:str];
                                             }];
